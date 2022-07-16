4845ca50380337bdea32d40d56522e034f9eee9d
Carlos Alcala <alcala21@gmail.com>
2022-07-16 15:17:52 -0500

    Stage 4/5. Healing potion.

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import sys
-from weather.api import key
+from weather.api import key, wurl
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+db = SQLAlchemy(app)
 
 
-@app.route("/", methods=['POST', 'GET'])
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    temperature = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<City {self.name}>"
+
+
+db.create_all()
+
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        city = request.form['city_name']
-        if city:
-            info = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={key}")
-            if info.status_code == 200:
-                data = info.json()
-                temperature = data['main']['temp']
-                state = data['weather'][0]['main']#
-                dict_with_weather_info = {"city": city, "temperature": temperature, "state": state}
-                return render_template("index.html", weather=dict_with_weather_info)
-    return render_template("index.html", weather=None)
+    city_list = City.query.all()
+    return render_template("index.html", city_list=city_list)
+
+
+@app.route("/add", methods=['POST'])
+def add_city_weather():
+    city_name = request.form.get('city_name')
+    wparams = {'q': city_name, 'appid': key, 'units': 'metric'}
+    info = requests.get(wurl, params=wparams)
+    if info.status_code == 200:
+        data = info.json()
+        loc_city = City(
+                        name = city_name,
+                        temperature = int(data['main']['temp']),
+                        state = data['weather'][0]['main']
+                        )
+        db.session.add(loc_city)
+        db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- weather/api.py
+++ weather/api.py
@@ -1,1 +1,5 @@
+wurl = "https://api.openweathermap.org/data/2.5/weather"
 key = "858120cb694d5bcbc01f4cbbef003509"
+
+
+
