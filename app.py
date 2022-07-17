37c61f90c5973fd5a0b9735b4c34b366f2099b83
Carlos Alcala <alcala21@gmail.com>
2022-07-16 23:54:21 -0500

    Stage 5/5. Wrong City.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 import requests
 import sys
 from weather.api import key, wurl
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SECRET_KEY'] = 'mi-clave-secreta'
 db = SQLAlchemy(app)
 
 
@@ -24,26 +25,44 @@ db.create_all()
 
 @app.route("/")
 def index():
-    city_list = City.query.all()
-    return render_template("index.html", city_list=city_list)
+    return render_template("index.html", cities=City.query.all())
 
 
 @app.route("/add", methods=['POST'])
 def add_city_weather():
     city_name = request.form.get('city_name')
+
+    if not city_name:
+        flash("The city doesn't exist!")
+        return redirect('/')
+
+    if City.query.filter_by(name=city_name).first():
+        flash("The city has already been added to the list!")
+        return redirect('/')
+
     wparams = {'q': city_name, 'appid': key, 'units': 'metric'}
     info = requests.get(wurl, params=wparams)
     if info.status_code == 200:
         data = info.json()
         loc_city = City(
-                        name = city_name,
-                        temperature = int(data['main']['temp']),
-                        state = data['weather'][0]['main']
+                        name=city_name,
+                        temperature=int(data['main']['temp']),
+                        state=data['weather'][0]['main']
                         )
         db.session.add(loc_city)
         db.session.commit()
+    else:
+        flash("The city doesn't exist!")
 
-    return redirect(url_for('index'))
+    return redirect("/")
+
+
+@app.route('/delete/<city_id>', methods=['POST'])
+def delete(city_id):
+    city = City.query.filter_by(id=city_id).first()
+    db.session.delete(city)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == '__main__':
